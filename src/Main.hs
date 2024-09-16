9081f9fe5ff8641b66c0fae479971768fab03297
Le0-dot <lkolyadich@gmail.com>
2024-09-16 18:35:56 +0200

    Correct indentation sensetive 'do' block. Use State to store current indentation.

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,25 @@
 module Main where
 
 import Data.Text.IO qualified as T
+import Data.Text qualified as T
+import Data.Void (Void)
+import System.Environment (getArgs)
+import Text.Megaparsec (MonadParsec (eof), between, errorBundlePretty, runParser, ParseErrorBundle)
+import Text.Megaparsec.Pos (mkPos)
+import Control.Monad.State (runStateT)
+import Types
 import Lexeme
 import Parser
-import System.Environment (getArgs)
-import Text.Megaparsec (MonadParsec (eof), between, errorBundlePretty, parse)
+
+parse :: Parser a -> String -> T.Text -> Either (ParseErrorBundle T.Text Void) (a, ParserState)
+parse parser = runParser (runStateT parser (mkPos 1))
 
 main :: IO ()
 main = do
     [file] <- getArgs
     contents <- T.readFile file
-    let out = parse (between spaceConsumer eof parseModule) file contents
+    let parser = between spaceConsumer (spaceConsumer >> eof) parseModule
+    let out = parse parser file contents
     case out of
         Left err -> putStrLn $ errorBundlePretty err
         Right res -> print res
