6ae0d68e6f5eed9ad88b942676f564ddd31fd2ee
Le0-dot <lkolyadich@gmail.com>
2024-09-23 16:47:18 +0200

    Add inference for BlockExpression, LetExpression and AssignmentExpression.

--- src/TypeDeduction/Inference.hs
+++ src/TypeDeduction/Inference.hs
@@ -9,9 +9,9 @@ inferExpression (ConstantExpression c) = inferConstant c
 inferExpression (IdentifierExpression i) = inferIdentifier i
 inferExpression (FunctionExpression f) = inferFunction f
 inferExpression (CallExpression c) = inferCall c
-inferExpression (BlockExpression b) = undefined
-inferExpression (LetExpression l) = undefined
-inferExpression (AssignmentExpression a) = undefined
+inferExpression (BlockExpression b) = inferBlock b
+inferExpression (LetExpression l) = inferLet l
+inferExpression (AssignmentExpression a) = inferAssignment a
 
 inferConstant :: Constant -> Inference Type
 inferConstant (IntegerConstant _) = return IntegerType
@@ -36,3 +36,21 @@ inferCall (Call callee args) = do
     let appliedCalleeType = foldr FunctionType resultType argTypes
     addConstraint $ TypeConstraint calleeType appliedCalleeType
     return resultType
+
+inferBlock :: [Expression] -> Inference Type
+inferBlock [] = undefined -- blocks are never empty, and if they are than parser is failing
+inferBlock [expr] = inferExpression expr
+inferBlock (expr:rest) = inferExpression expr >> inferBlock rest
+
+-- TODO: Add TypeVariable polimorphism to functions
+inferLet :: Assignment -> Inference Type
+inferLet (Assignment variable expression) = do
+    exprType <- inferExpression expression
+    envInsert variable exprType
+
+inferAssignment :: Assignment -> Inference Type
+inferAssignment (Assignment variable expression) = do
+    varType <- inferIdentifier variable
+    exprType <- inferExpression expression
+    addConstraint $ TypeConstraint varType exprType
+    return varType
